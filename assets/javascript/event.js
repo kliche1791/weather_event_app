cd7c68491cd901fb50941084aac9a3362547edcf
Firuza Abramyan <firuzaa8@gmail.com>
2019-11-29 11:44:58 -0500

    sending what i have so far

---
+++ assets/javascript/event.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+
+  $("#search").on("click", function () {
+    event.preventDefault();
+    
+    var eventLocation = $("#locationBox").val();
+    var eventQueryURL = "https://api.eventful.com/json/events/search?location=" + eventLocation + "&page_size=10&date=today&app_key=ZQ6TgSgf8HWBSqw3";
+  
+    console.log(eventQueryURL);
+    $.ajax({
+      url: eventQueryURL,
+      method: "GET",
+      crossDomain: true,
+      dataType: "jsonp"
+
+
+    }).then(function (response) {
+      console.log(response);
+      theEvents(response);
+      //var parsed = JSON.parse(response);
+      //console.log(response.total_items);
+      //console.log(eventQueryURL);
+      $("#locationBox").html("<h1>" + response.events.event[0].country_name + "</h1>");
+     
+      function theEvents(response) {
+        for (var i = 0; i < response.page_size; i++) {
+
+          var event = $("<div>");
+          event.addClass("eventCard card cardT");
+
+          var card = $("<div>");
+          card.addClass("card-header");
+          var n = i + 1;
+          card.text("EVENT # " + n);
+
+          event.append(card);
+          var eventNames = $("<h5>");
+          eventNames.addClass("eventName");
+          eventNames.append("TITLE: " + response.events.event[i].title);
+
+          var eventAddr = $("<h5>");
+          eventAddr.addClass("eventAddress");
+          eventAddr.append("Address: " + response.events.event[i].venue_address);
+          eventAddr.append(", " + response.events.event[i].city_name);
+          eventAddr.append(", " + response.events.event[i].region_abbr);
+
+
+          var dateTime = $("<h5>");
+          dateTime.addClass("eventDate");
+          dateTime.append("Start Time: " + response.events.event[i].start_time);
+          dateTime.append(", Finish Time: " + response.events.event[i].stop_time);
+
+          var nameVenue = $("<h5>");
+          nameVenue.addClass("eventVenue");
+          nameVenue.append("Name of the Venue: " + response.events.event[i].venue_name);
+
+          var urlEvent = $("<a>");
+          urlEvent.attr("href", response.events.event[i].url);
+          urlEvent.addClass("eventURL");
+          urlEvent.append("URL: " + response.events.event[i].url);
+
+          event.append(eventNames, eventAddr, dateTime, nameVenue, urlEvent);
+
+          $("#eventBox").append(event);
+        };
+      }
+    });
+
+  });
+
+});
